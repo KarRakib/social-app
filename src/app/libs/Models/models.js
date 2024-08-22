e6e2b2a7d679b757122bd854473102cd50b57449
Kar Rakib <karrakib@localhost.localdomain>
2024-08-22 12:42:07 +0100

    user send database model

--- src/app/libs/Models/models.js
+++ src/app/libs/Models/models.js
@@ -1,17 +1,54 @@
 import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
-  clerkId: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  profilePhoto: { type: String, required: true },
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SocialPosts', default: [] }],
-  savedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SocialPosts', default: [] }],
-  likedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SocialPosts', default: [] }],
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SocialUsers', default: [] }],
-  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SocialUsers', default: [] }],
-  createdAt: { type: Date, default: Date.now },
+  clerkId: {
+    type: String,
+    required: true,
+    uinque: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  profilePhoto: {
+    type: String,
+    required: true,
+  },
+  posts: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    default: [],
+  },
+  savedPosts: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    default: [],
+  },
+  likedPosts: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    default: [],
+  },
+  followers: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+  following: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const PostSchema = new mongoose.Schema({
