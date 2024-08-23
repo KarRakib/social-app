bf33995eae4ec7ddfbdb898aa0bb80a8b3bb8d1d
Kar Rakib <karrakib@localhost.localdomain>
2024-08-23 03:51:56 +0100

    ready for p

--- src/app/components/LeftSideBar.jsx
+++ src/app/components/LeftSideBar.jsx
@@ -36,9 +36,7 @@ const LeftSideBar = () => {
       }
   }, [user, isLoaded]);
 
-  return loading || !isLoaded ? (
-      <Loader />
-    ):(
+  return(
       <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
         <Image src="/user.jpg" alt="logo" width={200} height={200} />
