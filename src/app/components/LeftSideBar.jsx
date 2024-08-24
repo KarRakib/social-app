c86f05b59a1546c0eedb777843bad0a97fca231d
karrakib <rakibkazi777@gmail.com>
2024-08-24 08:05:41 +0600

    deploy issu

--- src/app/components/LeftSideBar.jsx
+++ src/app/components/LeftSideBar.jsx
@@ -36,7 +36,9 @@ const LeftSideBar = () => {
       }
   }, [user, isLoaded]);
 
-  return(
+  return loading || !isLoaded ? (
+    <Loader />
+  ) : (
       <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
         <Image src="/user.jpg" alt="logo" width={200} height={200} />
